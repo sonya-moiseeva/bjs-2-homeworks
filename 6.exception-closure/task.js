551660a36b852c3a8db5db5465453060e9f91d77
Дмитрий Моисеев <rukavishnikovasonya@mail.ru>
2023-04-15 11:31:06 +0700

    Все задачи реализованы

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+function parseCount(parseNumber) {
+  let result = Number.parseFloat(parseNumber, 10);
+  newError = new Error("Невалидное значение");
+  if (Number.isNaN(Number.parseFloat(parseNumber))) {
+    throw newError;
+  } else {
+    return result;
+  }
+}
+function validateCount(toParse) {
+  try {
+    let result = parseCount(toParse);
+    return result;
+  } catch (newError) {
+    console.log(newError);
+    return newError;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+  get area() {
+    let s = this.perimeter / 2;
+    return Number(
+        Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3)
+    );
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (err) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
